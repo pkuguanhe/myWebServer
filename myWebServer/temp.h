647794de15a2634b053fc09db06c1f90edbf08e9
pkuguanhe <guanhe@pku.edu.cn>
2019-03-24 15:50:01 +0800

    0.0

---
+++ myWebServer/temp.h
@@ -0,0 +1,27 @@
+﻿#include <csapp.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <signal.h>
+#include <dirent.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/mman.h>
+#include <math.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <sys/socket.h>
+#include <sys/epoll.h>
+#include <sys/select.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <iostream>
+using namespace std;
